6b1d10cb975ddd5d355821ef9dd15d23b13ad100
aaronlam1004 <aaronlam1004@gmail.com>
2021-07-06 16:06:22 -0700

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import './BodySection.css';
 function About() {
     return(
         <div className="sectionBody" id="about">
+            <h1>Aaron Lam</h1>
             <h1>About</h1>
             Hello. I am a Computer Science student from UCI. I am interested in a lot of things.
         </div>
